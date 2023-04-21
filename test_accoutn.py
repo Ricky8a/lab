924220f86791c2f0e8dafdc1cb8a59863886594e
Ricky8a <ludic882002@gmail.com>
2023-04-20 19:27:55 -0500

    Added second file

--- test_accoutn.py
+++ test_accoutn.py
@@ -1,5 +1,5 @@
 import pytest
-from account import *
+from account import Account
 
 class Test:
     def setup_method(self):
@@ -9,12 +9,37 @@ class Test:
         del self.a1
 
     def test_init(self):
-        pass
+        assert self.a1.get_name() == 'John'
+        assert self.a1.get_balance() == 0.0
 
     def test_deposit(self):
-        #negative, zero, positive
-        pass
+        # Test with negative amount
+        assert self.a1.deposit(-100) == False
+        assert self.a1.get_balance() == 0.0
+
+        # Test with zero amount
+        assert self.a1.deposit(0) == False
+        assert self.a1.get_balance() == 0.0
+
+        # Test with positive amount
+        assert self.a1.deposit(500) == True
+        assert self.a1.get_balance() == 500.0
 
     def test_withdraw(self):
-        # negative, zero, positive invalid, positive valid
-        pass
+        # Test with negative amount
+        assert self.a1.withdraw(-100) == False
+        assert self.a1.get_balance() == 0.0
+
+        # Test with zero amount
+        assert self.a1.withdraw(0) == False
+        assert self.a1.get_balance() == 0.0
+
+        # Test with amount greater than balance
+        assert self.a1.withdraw(1000) == False
+        assert self.a1.get_balance() == 0.0
+
+        # Test with valid withdrawal
+        assert self.a1.deposit(500) == True
+        assert self.a1.get_balance() == 500.0
+        assert self.a1.withdraw(200) == True
+        assert self.a1.get_balance() == 300.0
